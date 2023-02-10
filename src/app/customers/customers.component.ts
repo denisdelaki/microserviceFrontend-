7a2d07c8380b52405beffe4e592cbe07b43a09a0
denisdelaki <denisdelaki@gmail.com>
2023-02-10 13:46:57 +0300

    fetched the customer data from the backend

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent {
-
+export class CustomersComponent implements OnInit {
+customers: any;
+constructor (private http: HttpClient){}
+ngOnInit (){
+  this.http.get('http://localhost:3000/customers.json').subscribe(data=>{
+    this.customers =data
+  })
+}
 }
